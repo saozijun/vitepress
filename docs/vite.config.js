d06bbb94944fb2bb1193051d63480c9e2c5b9cb0
saozijun <1922708711@qq.com>
2023-02-21 20:40:23 +0800

    no message

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,7 +3,7 @@ import { SearchPlugin } from "vitepress-plugin-search";
 import { defineConfig } from "vite";
 import flexSearchIndexOptions from "flexsearch";
 //default options
-var options = {
+const options = {
   ...flexSearchIndexOptions,
   previewLength: 100, //搜索结果预览长度
   buttonLabel: "搜索",
