060afca2e0d040dd7ec9b3a21ddb3065f9c96ff2
人才太美 <9265505+talent-is-too-beautiful@user.noreply.gitee.com>
2023-10-13 23:18:59 +0800

    优化修改

--- docs/view/home.vue
+++ docs/view/home.vue
@@ -6,10 +6,14 @@
 		<div v-else class="image-container">
 			<p class="logo-text">Volcano</p>
 		</div>
-		<button class="btn svg-wrapper">
-			<span><a href="/web/FrontEnd/tutorial">Start</a></span>
-			
-		</button>
+		<div class="btn_box">
+			<a href="/web/FrontEnd/tutorial.html">
+				<button data-text="Awesome" class="button">
+					<span class="actual-text">&nbsp;start&nbsp;</span>
+					<span class="hover-text" aria-hidden="true">&nbsp;start&nbsp;</span>
+				</button>
+			</a>
+		</div>
 	</div>
 	<div class="about">
 		<div class="about_title">📕 笔记</div>
@@ -37,7 +41,6 @@ onMounted(() => {
 })
 
 const curDeviceIsMobile = ref(false)
-
 onBeforeMount(() => {
 	curDeviceIsMobile.value = isMobile();
 });
@@ -60,83 +63,57 @@ onBeforeMount(() => {
 		z-index: -1;
 	}
 
-	.svg-wrapper {
+	.btn_box {
 		position: absolute;
-		width: 190px;
-		height: 60px;
-		text-align: center;
-		line-height: 40px;
-		cursor: pointer;
-		transform: translate(-50%, -50%);
+		top: 80%;
 		left: 50%;
-		bottom: 20%;
-		z-index: 9999;
-	}
-
-	.btn {
-		border: none;
-		position: relative;
-		width: 200px;
-		height: 73px;
-		padding: 0;
-		z-index: 2;
-		-webkit-mask: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='868' width='2500' viewBox='0 0 726 252.17'%3E%3Cpath d='M483.92 0S481.38 24.71 466 40.11c-11.74 11.74-24.09 12.66-40.26 15.07-9.42 1.41-29.7 3.77-34.81-.79-2.37-2.11-3-21-3.22-27.62-.21-6.92-1.36-16.52-2.82-18-.75 3.06-2.49 11.53-3.09 13.61S378.49 34.3 378 36a85.13 85.13 0 0 0-30.09 0c-.46-1.67-3.17-11.48-3.77-13.56s-2.34-10.55-3.09-13.61c-1.45 1.45-2.61 11.05-2.82 18-.21 6.67-.84 25.51-3.22 27.62-5.11 4.56-25.38 2.2-34.8.79-16.16-2.47-28.51-3.39-40.21-15.13C244.57 24.71 242 0 242 0H0s69.52 22.74 97.52 68.59c16.56 27.11 14.14 58.49 9.92 74.73C170 140 221.46 140 273 158.57c69.23 24.93 83.2 76.19 90 93.6 6.77-17.41 20.75-68.67 90-93.6 51.54-18.56 103-18.59 165.56-15.25-4.21-16.24-6.63-47.62 9.93-74.73C656.43 22.74 726 0 726 0z'/%3E%3C/svg%3E") no-repeat 50% 50%;
-		mask: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='868' width='2500' viewBox='0 0 726 252.17'%3E%3Cpath d='M483.92 0S481.38 24.71 466 40.11c-11.74 11.74-24.09 12.66-40.26 15.07-9.42 1.41-29.7 3.77-34.81-.79-2.37-2.11-3-21-3.22-27.62-.21-6.92-1.36-16.52-2.82-18-.75 3.06-2.49 11.53-3.09 13.61S378.49 34.3 378 36a85.13 85.13 0 0 0-30.09 0c-.46-1.67-3.17-11.48-3.77-13.56s-2.34-10.55-3.09-13.61c-1.45 1.45-2.61 11.05-2.82 18-.21 6.67-.84 25.51-3.22 27.62-5.11 4.56-25.38 2.2-34.8.79-16.16-2.47-28.51-3.39-40.21-15.13C244.57 24.71 242 0 242 0H0s69.52 22.74 97.52 68.59c16.56 27.11 14.14 58.49 9.92 74.73C170 140 221.46 140 273 158.57c69.23 24.93 83.2 76.19 90 93.6 6.77-17.41 20.75-68.67 90-93.6 51.54-18.56 103-18.59 165.56-15.25-4.21-16.24-6.63-47.62 9.93-74.73C656.43 22.74 726 0 726 0z'/%3E%3C/svg%3E") no-repeat 50% 50%;
-		-webkit-mask-size: 100%;
-		cursor: pointer;
-		background-color: transparent;
-		transform: translateY(8px)
-	}
-
-	.btn:after {
-		content: '';
-		position: absolute;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		box-shadow: 0px 0 0 0 white;
-		transition: all 2s ease;
-	}
-
-	.btn:hover:after {
-		box-shadow: 0px -13px 56px 12px #ffffffa6;
-	}
-
-	.btn span a {
-		position: absolute;
-		width: 100%;
-		font-size: 20px;
-		font-weight: bold;
-		left: 50%;
-		top: 39%;
-		letter-spacing: 3px;
-		text-align: center;
 		transform: translate(-50%, -50%);
-		color: #7ccfff;;
-		transition: all 2s ease;
+
+		.button {
+			margin: 0;
+			height: auto;
+			background: transparent;
+			padding: 0;
+			border: none;
+
+		}
+
+		/* button styling */
+		.button {
+			--border-right: 6px;
+			--text-stroke-color: rgba(255, 255, 255, 0.6);
+			--animation-color: #5bbdff;
+			--fs-size: 2em;
+			letter-spacing: 3px;
+			text-decoration: none;
+			font-size: var(--fs-size);
+			font-family: "Arial";
+			position: relative;
+			text-transform: uppercase;
+			color: transparent;
+			-webkit-text-stroke: 1px var(--text-stroke-color);
+		}
+
+		/* this is the text, when you hover on button */
+		.hover-text {
+			position: absolute;
+			box-sizing: border-box;
+			content: attr(data-text);
+			color: var(--animation-color);
+			width: 0%;
+			inset: 0;
+			border-right: var(--border-right) solid var(--animation-color);
+			overflow: hidden;
+			transition: 0.5s;
+			-webkit-text-stroke: 1px var(--animation-color);
+		}
+
+		/* hover */
+		.button:hover .hover-text {
+			width: 100%;
+			filter: drop-shadow(0 0 23px var(--animation-color))
+		}
 	}
-
-	.btn:hover span a {
-		color: white;
-	}
-
-	.btn:before {
-		content: '';
-		position: absolute;
-		width: 0;
-		height: 100%;
-		background-color: #0b0c11;;
-		left: 50%;
-		top: 50%;
-		transform: translate(-50%, -50%);
-		transition: all 1s ease;
-	}
-
-	.btn:hover:before {
-		width: 100%;
-	}
-
-
 }
 
 .image-container {
